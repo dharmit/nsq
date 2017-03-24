97b6567c654a94f702fb6b321d2d654fb370e580
Dharmit Shah <dshah@fedora.redhat.com>
2017-03-24 18:41:04 +0530

    Build NSQ images based on CentOS through the Pipeline

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/nsqio/nsq/releases/download/v1.0.0-compat/nsq-1.0.0-compat.linux-amd64.go1.8.tar.gz
+tar xvf nsq-1.0.0-compat.linux-amd64.go1.8.tar.gz && rm -f nsq-1.0.0-compat.linux-amd64.go1.8.tar.gz
+cp nsq-1.0.0-compat.linux-amd64.go1.8/bin/*nsq* /usr/local/bin/
